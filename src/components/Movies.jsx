9a95f61255abe7bfbe56a1c77e3051b29ed47626
Yansey Dominguez <45616244+Yanseydc@users.noreply.github.com>
2023-05-15 00:37:40 -0700

    adding movies message

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,6 +4,11 @@ import MoviesList from './MoviesList'
 function Movies ({ movies }) {
   return (
     <div className='movies'>
+      {
+        movies.length === 0
+          ? (<p className='movies__message'>No movies to show, please search for a movie</p>)
+          : null
+      }
       <MoviesList movies={movies} />
     </div>
   )
